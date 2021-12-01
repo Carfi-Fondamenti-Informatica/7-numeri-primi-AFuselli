d6fc8d7a1850eaedfac4e05c3892b2fb3ebf18ad
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-12-01 21:50:19 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
-
+#include "lib.h"
+bool NumeriPrimi(int n,int d){
+    int rest=0;
+    if (n>1){
+        if (d<n){
+            rest=n%d;
+            if (rest != 0) {
+                return NumeriPrimi(n, d + 1);
+            }else{
+                return false;
+            }
+        } else{
+            return true;
+        }
+    } else if (n==2){
+        return true;
+    } else {
+        return false;
+    }
+}
