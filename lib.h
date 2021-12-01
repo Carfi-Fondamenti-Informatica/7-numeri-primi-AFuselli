142cd539befdafe536bced13fc005bae9cba9338
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-12-01 21:50:55 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
+//
+// Created by Andrea Fuselli on 01/12/21.
+//
+
+#ifndef COMPITO7_NUMERIPRIMI_LIB_H
+#define COMPITO7_NUMERIPRIMI_LIB_H
+
+bool NumeriPrimi(int , int);
+
+#endif //COMPITO7_NUMERIPRIMI_LIB_H
 
